ca67e0b87a0afd7fa18d2bfec5c9308eea21c79d
Zhu Yuncheng <chuwanshing@gmail.com>
2018-12-05 21:25:02 +0800

    [feature] kill csdn check adblock login

--- Readme.md
+++ Readme.md
@@ -4,5 +4,11 @@
 ## 测试安装
 Chrome浏览器地址栏中输入`chrome://extensions/`, 右上角打开`开发者模式`, 通过`加载已解压的扩展程序`按钮加载该项目,  然后你就可以安静的访问CSDN了
 
+## AdBlock
+如果你用AdBlock来拦截广告, 该死的CSDN会监测到, 顺便我也把它也给干掉了, 放心食用
+
 ## 为什么不发布到`chrome web store`
 嘤嘤嘤, 发布到`chrome web store`要5美元, 所以我没钱
+
+## 致谢
+感谢CSDN提供平台, 让我学习开发知识, 然后把CSND给hack掉, HHHA~
---
+++ js/background.js
@@ -0,0 +1,26 @@
+
+chrome.webRequest.onBeforeRequest.addListener(
+  /**
+   * 
+   * @param {*} details 
+   * {
+   *    "frameId": 0,
+   *    "initiator": "https://blog.csdn.net",
+   *    "method": "GET",
+   *    "parentFrameId": -1,
+   *    "requestId": "9595",
+   *    "tabId": 1462039504,
+   *    "timeStamp": 1544012791315.46,
+   *    "type": "stylesheet",
+   *    "url": "https://g.csdnimg.cn/check-adblock/1.1.1/css/check-adblock.css"
+   * }
+   */
+  function (details) {
+    if (details.url.match(/check\-adblock/)) {
+      details.url = ""
+      return { cancel: true }
+    }
+  },
+  { urls: ["<all_urls>"] },
+  ["blocking"]
+);
